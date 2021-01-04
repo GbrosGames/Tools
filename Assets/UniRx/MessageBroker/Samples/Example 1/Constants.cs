6940b9227d2d264f1d95455ea9c693e74ac82cce
nol1fe <patryk.grech@hotmail.com>
2021-01-04 17:20:54 +0100

    package.json update

--- Assets/UniRx/MessageBroker/Samples/Example 1/Constants.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Gbros.UniRx.MessageBrokerSamples.Example1
-{
-    public class Constants
-    {
-        public const string ComponentPath = "MessageBrokerSamples/Example 1/";
-    }
-}
--- Assets/UniRx/MessageBroker/Samples/Example 3/ZoneType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Gbros.UniRx.MessageBrokerSamples.Example3
-{
-    public enum ZoneType
-    {
-        Default,
-        Danger,
-        HiddenTreasure
-    }
-}
