a1f12d24502132c148e020bf1edd1a5b898f0dff
nol1fe <patryk.grech@hotmail.com>
2022-04-29 13:42:59 +0200

    Update Watcher Samples

---
+++ Assets/Watchers/Examples~/Example 1/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace Gbros.Watchers.Samples.Example2
+{
+    public class Constants
+    {
+        public const string ComponentPath = "_Watchers/Example 1/";
+    }
+}
---
+++ Assets/Watchers/Examples~/Example 1/SomeComponent.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Gbros.Watchers.Samples.Example2;
+using UnityEngine;
+
+namespace Gbros.Watchers.Samples.Example1
+{
+    [Serializable]
+    public class SomeClass
+    {
+        public Color SomeColor;
+        public string SomeText;
+    }
+
+    public class SomeNotUnityObjectClass
+    {
+        public int SomeValue { get; set; }
+    }
+
+
+    [AddComponentMenu(Constants.ComponentPath + nameof(SomeComponent))]
+    public class SomeComponent : MonoBehaviour
+    {
+
+        public int SomeValue = 2;
+        public Vector2 SomeVector2 = Vector2.up;
+        public Vector3 SomeVector3 = Vector3.up;
+        public SomeClass SomeClass;
+
+        private void Awake()
+        {
+            var someNotUnityObjectClass = new SomeNotUnityObjectClass
+            {
+                SomeValue = 123
+            };
+
+#if UNITY_EDITOR
+            Watchers.Watcher("Watcher A")
+                           .Board("Board A")
+                           .Card("Card A")
+                           .Container("Container A")
+                           .AddSelectorButton($"Select {gameObject.name}", gameObject)
+                           .AddSerializedProperty(this, x => x.SomeValue)
+                           .AddSerializedProperty(this, "SomeVector2")
+                           .AddSerializedProperty(this, x => x.SomeVector3)
+                           .AddSerializedProperty(this, x => x.SomeClass)
+                           .AddProperty("SomeValue in someNonSerializableClass", someNotUnityObjectClass, x => x.SomeValue, x => someNotUnityObjectClass.SomeValue = x)
+                           .AddActionButton("Some Action", () => { Debug.Log($"Value from nonUnityObjectClass - {someNotUnityObjectClass.SomeValue}"); });
+#endif
+        }
+    }
+
+    public class WatcherInitializer
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+    public static void Initialize()
+    {
+        // Watchers.EditorStylePath = "Assets/MyStyles.uss";
+    }
+}
+}
